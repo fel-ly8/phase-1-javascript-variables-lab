dec739616cc5fc89d6b2479d40c847a683d92939
felister kiragu <kiragufelister18@gmail.com>
2024-09-24 08:55:33 +0300

    add index.html

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const companyName = ("Scuber")
-console.log(companyName)
+
 
 let mostProfitableNeighborhood = ("chelsea")
-console.log(mostProfitableNeighborhood)
+
 
 
 let companyCeo = ("Susan Smith")
-console.log(companyCeo)
+
 
